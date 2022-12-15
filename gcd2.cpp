9e56a6fb27809494544c2e3ba7a333d47ef01c8d
Moiz Ahmed <moizahmedbss786@gmail.com>
2022-12-16 03:51:32 +0500

    Add files via upload

---
+++ gcd2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b)
+{
+	if (b == 0)
+	{
+		return a;
+	}
+	return gcd(b, a % b);
+}
+int main()
+{
+	int a = 0, b = 0;
+	cin >> a >> b;
+	cout << gcd(a, b) << endl;
+}
